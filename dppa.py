b623ffc348345ba092e05002120c968b19d24a5c
Jan Justi <9977169+janjusti@users.noreply.github.com>
2019-05-05 14:50:10 -0300

    added argparser (still on main)

--- dppa.py
+++ dppa.py
@@ -1,6 +1,16 @@
 ### Deep Protein Polarity Analyser (DPPA) v0.1
-
+import argparse
 from src.helpers.timer import CustomTimer
 
-# initialize timer
-c_timer = CustomTimer()
+def main(args):
+    # initialize timer
+    c_timer = CustomTimer()
+
+
+if __name__ == '__main__':
+    # get options from user
+    parser = argparse.ArgumentParser(description='Analyse all protein alignment .fasta files.')
+    parser.add_argument('-i', '--input', help='Input folder with .fasta files.', required=True)
+    parser.add_argument('-t', '--target', help='Target .fasta file to be analysed.', required=True)
+    user_args = vars(parser.parse_args())
+    main(user_args)
