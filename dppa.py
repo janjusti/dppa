ec66d8f41373aa7d49909a31c6413a7773d6664c
Jan Justi <9977169+janjusti@users.noreply.github.com>
2019-05-11 21:06:24 -0300

    fixed loads of stuff (I know I should be specific)

--- dppa.py
+++ dppa.py
@@ -17,30 +17,41 @@ def main(args):
     # create dfs
     df_pol_results = pd.DataFrame(columns=['ColNum','PossibleAminos','PossiblePols', 'PolScore'])
     df_alert_results = pd.DataFrame(columns=['SeqName','ColNum','AlertType'])
+    df_pols = pd.read_csv(Path.cwd() / 'src' / 'conv' / 'pols.csv')
+    # get list of unknown and known aminos
+    [unknown_aminos, known_aminos] = auxf_handler.get_lists_aminos(df_pols)
     # execute deep searcher 
     number_columns = len(target_list[0][1]) # number of chars on sequence
     # on each column from .fasta target file
     for current_col in tqdm(range(0, number_columns)):
         root, df_alert_results = auxf_handler.deep_searcher(
-            args['input'], target_list, current_col, df_alert_results
+            args['input'], target_list, current_col, df_alert_results, unknown_aminos
         )
         # get root list of elements
         amino_leaves = []
         for leaf in root.leaves:
-            amino_leaves.append(leaf.amino)
-        # remove unlisted chars
-        blacklisted_str = ['-', 'X', 'B', 'Z', '?']
-        amino_leaves = [x for x in amino_leaves if x not in blacklisted_str]
+            if leaf.amino in known_aminos or leaf.amino in unknown_aminos:
+                amino_leaves.append(leaf.amino)
+            else:
+                # invalid char, increment on df_alert_results
+                df_alert_results = auxf_handler.add_alert_on_df(
+                    df_alert_results,
+                    leaf.name,
+                    current_col,
+                    'Unlisted Char ' + leaf.amino
+                )
+        # remove still unknown aminos
+        amino_leaves = [x for x in amino_leaves if x not in unknown_aminos]
         # unify list
         unified_list = list(set(amino_leaves))
         # check length
         if len(unified_list) > 1:
             # get aminos percentages dict
-            aminos_dict = auxf_handler.get_aminos_perc_dict(root, unified_list)
+            aminos_dict = auxf_handler.get_aminos_perc_dict(root, unified_list, unknown_aminos)
             # get polarities percentages dict 
-            pols_dict = auxf_handler.get_polarities_perc_dict(aminos_dict)
+            pols_dict = auxf_handler.get_polarities_perc_dict(aminos_dict, df_pols)
             # get polarity score
-            curr_pol_score = auxf_handler.get_pol_score(pols_dict)
+            curr_pol_score = auxf_handler.get_pol_score(pols_dict, df_pols)
             # increment on df_pol_results
             df_pol_results = df_pol_results.append(
                 {
--- src/helpers/auxfunc.py
+++ src/helpers/auxfunc.py
@@ -2,18 +2,47 @@ from pathlib import Path
 from Bio.SeqIO import FastaIO
 from anytree import AnyNode, LevelOrderGroupIter
 import pandas as pd
+import string
 
 class AuxFuncPack:
     # package of custom functions
 
     def fasta_to_list(self, fastas_folder, fasta_fn):
-        # convert .fasta file into a list
+        ### convert .fasta file into a list
         # list format: [(name, seq)]
         filepath = Path.cwd() / 'batch' / fastas_folder / fasta_fn
         fasta_list = list(FastaIO.SimpleFastaParser(open(filepath, 'rU')))
+
         return fasta_list
 
-    def deep_searcher(self, fastas_folder, fasta_list, col_num, df_alert):
+    def add_alert_on_df(self, df_alert, seq_name, col_num, alert_type_str):
+        df_alert = df_alert.append(
+            {
+                'SeqName': seq_name.replace('_', ' '),
+                'ColNum': col_num+1,
+                'AlertType': alert_type_str
+            }
+        , ignore_index=True)
+
+        return df_alert
+    
+    def get_lists_aminos(self, df_pols):
+        ### get list of unknown aminos, based on df_pols
+        # get alphabet list
+        unknown_aminos = list(string.ascii_uppercase)
+        # add '?' and '-' char
+        unknown_aminos.extend(['?', '-'])
+        # start known aminos list
+        known_aminos = []
+        # remove all pol-defined letters
+        for curr_pol in range(0, df_pols.shape[0]):
+            list_curr_pol = list(df_pols.Amino.at[curr_pol])
+            known_aminos.extend(list_curr_pol)
+            unknown_aminos = [x for x in unknown_aminos if x not in list_curr_pol]
+
+        return unknown_aminos, known_aminos
+
+    def deep_searcher(self, fastas_folder, fasta_list, col_num, df_alert, unknown_aminos):
         # create local root
         loc_root = AnyNode(name='LocRoot', amino='LocRootAmino')
         # create nodes from column number
@@ -26,16 +55,7 @@ class AuxFuncPack:
         # run through nodes
         for node in loc_root.children:
             # check amino cases
-            if node.amino is '-':
-                # increment on df_alert
-                df_alert = df_alert.append(
-                    {
-                        'SeqName': node.name.replace('_', ' '),
-                        'ColNum': col_num+1,
-                        'AlertType': 'Gap'
-                    }
-                , ignore_index=True)
-            if node.amino in ['X', 'B', 'Z', '?']:
+            if node.amino in unknown_aminos:
                 if 'consensus_sequence' in node.name: 
                     fasta_dict = dict(fasta_list)
                     # check previous number of gaps from sequence
@@ -47,25 +67,22 @@ class AuxFuncPack:
                     deeper_fn = deeper_fn.replace(' consensus sequence','') + '.fasta'
                     deeper_list = self.fasta_to_list(fastas_folder, deeper_fn)
                     # go deeper
-                    deeper_result = self.deep_searcher(fastas_folder, deeper_list, shifted_col_num, df_alert)
+                    deeper_result = self.deep_searcher(fastas_folder, deeper_list, shifted_col_num, df_alert, unknown_aminos)
                     deeper_result[0].parent = node
                     df_alert = deeper_result[1]
                 else:
-                    # pure sequence, increment on df_alert
-                    pure_str = 'Pure ' + node.amino
-                    df_alert = df_alert.append(
-                        {
-                            'SeqName': node.name.replace('_', ' '),
-                            'ColNum': col_num+1,
-                            'AlertType': pure_str
-                        }
-                    , ignore_index=True)
-                    pass
+                    # non-deepable char, increment on df_ale
+                    df_alert = self.add_alert_on_df(
+                        df_alert, 
+                        node.name,
+                        col_num, 
+                        'Pure ' + node.amino
+                    )
 
         return loc_root, df_alert
 
-    def get_aminos_perc_dict(self, root, aminos_list):
-        # convert list of aminos to dict (amino -> its perc)
+    def get_aminos_perc_dict(self, root, aminos_list, unknown_aminos):
+        ### convert list of aminos to dict (amino -> its perc)
         # get list of aminos levels
         aminos_levels = [[node.amino for node in children] for children in LevelOrderGroupIter(
             root,
@@ -74,7 +91,9 @@ class AuxFuncPack:
         # clear any empty lists (parent roots)
         aminos_levels = [x for x in aminos_levels if x]
         # run through levels
-        deepables = ['X', 'B', 'Z']
+        deepables = [*unknown_aminos]
+        deepables.remove('?') # '?' is not deepable
+        deepables.remove('-') # '-' is not deepable
         aminos_dict = {}
         for target_amino in aminos_list:
             total_perc = 0
@@ -84,15 +103,13 @@ class AuxFuncPack:
                 total_perc = total_perc + (amino_perc_on_lvl)*deepness_perc
                 num_of_deepables = sum(level.count(x) for x in deepables)
                 deepables_perc_on_lvl = (num_of_deepables/len(level))
-                deepness_perc = deepness_perc * deepables_perc_on_lvl                
+                deepness_perc = deepness_perc * deepables_perc_on_lvl
             aminos_dict[target_amino] = round(100*total_perc,3)
 
         return aminos_dict
     
-    def get_polarities_perc_dict(self, aminos_dict):
-        # get dict of polarities within aminos list
-        filepath = Path.cwd() / 'src' / 'conv' / 'pols.csv'
-        df_pols = pd.read_csv(filepath)
+    def get_polarities_perc_dict(self, aminos_dict, df_pols):
+        ### get dict of polarities within aminos list
         # get list of tuples (pol_type -> perc)
         list_percs = []
         for curr_pol in range(0, df_pols.shape[0]):
@@ -107,13 +124,10 @@ class AuxFuncPack:
 
         return pols_dict
 
-    def get_pol_score(self, pols_dict):
-        # calculate polarity score
+    def get_pol_score(self, pols_dict, df_pols):
+        ### calculate polarity score
         # convert dict into list of tuples
         pols_percs = list(pols_dict.items())
-        # get table of polarity scores
-        filepath = Path.cwd() / 'src' / 'conv' / 'pols.csv'
-        df_pols = pd.read_csv(filepath)
         # get sum of current scores
         sum_scores = 0
         for curr_pol in pols_percs:
@@ -126,14 +140,15 @@ class AuxFuncPack:
             # get minimal value of perc
             min_pols = min(pols_percs, key = lambda t: t[1])[1]
             pol_score = round(sum_scores*3 + pol_list_size*0.6 + min_pols*0.01, 5)
+
         return pol_score
 
     def export_dfs(self, folder_name, target_name, df_alert_results, df_pol_results):
-        # export alerts and pols df to csv
+        ### export alerts and pols df to csv
         filepath = Path.cwd() / 'batch' / folder_name / target_name
         alerts_filepath = str(filepath).replace('.fasta', '') + '-alerts.csv'
         pols_filepath = str(filepath).replace('.fasta', '') + '-pols.csv'
-        print('Exporting alerts...')
+        print('Exporting alerts... / Total:', df_alert_results.shape[0])
         df_alert_results.to_csv(alerts_filepath, index=False)
         print('Exporting polarity results...')
         df_pol_results.to_csv(pols_filepath, index=False) 
