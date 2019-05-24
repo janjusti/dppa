0873e2dfb3a95d322fc43cc2cea339337c75e3cd
Jan Justi <9977169+janjusti@users.noreply.github.com>
2019-05-24 01:34:54 -0300

    script name changed

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ setup(
     include_package_data=True,
     entry_points={
         'console_scripts': [
-            'rundppa = dppa.core:main'
+            'run-dppa = dppa.core:main'
         ]
     }
 )
