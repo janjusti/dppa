d42447590e30c5abf6030fe94052ee6b64d29704
Jan Justi <9977169+janjusti@users.noreply.github.com>
2019-05-25 00:31:38 -0300

    hiding src folder from import (at least trying)

--- dppa/core.py
+++ dppa/core.py
@@ -1,7 +1,7 @@
 ### Deep Protein Polarity Analyser (DPPA)
 ### Copyright (c) 2019 Jan Marans Agnella Justi & Mariana de França Costa
 
-from .src import _core_methods
+from ._src import _core_methods
 
 _cmh = _core_methods.CoreMethods()
 
